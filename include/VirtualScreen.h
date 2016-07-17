5076b2e0da2dbd70449926029a1f399f422f0ab1
Amish Naidu <amhndu@gmail.com>
2016-07-17 16:44:22 +0530

    VirtualScreen pixel width is now float

--- include/VirtualScreen.h
+++ include/VirtualScreen.h
@@ -9,14 +9,14 @@ namespace sn
     class VirtualScreen : public sf::Drawable
     {
     public:
-        void create (uint width, uint height, uint pixel_size, sf::Color default_color);
+        void create (uint width, uint height, float pixel_size, sf::Color default_color);
         void setPixel (std::size_t x, std::size_t y, sf::Color color);
 
     private:
         void draw(sf::RenderTarget& target, sf::RenderStates states) const;
 
         sf::Vector2<uint> m_screenSize;
-        uint m_pixelSize; //virtual pixel size in real pixels
+        float m_pixelSize; //virtual pixel size in real pixels
         sf::VertexArray m_vertices;
     };
 };
--- src/VirtualScreen.cpp
+++ src/VirtualScreen.cpp
@@ -2,7 +2,7 @@
 
 namespace sn
 {
-    void VirtualScreen::create(uint w, uint h, uint pixel_size, sf::Color default_color)
+    void VirtualScreen::create(uint w, uint h, float pixel_size, sf::Color default_color)
     {
         m_vertices.resize(w * h * 6);
         m_screenSize = {w, h};
@@ -20,8 +20,7 @@ namespace sn
     void VirtualScreen::setPixel(std::size_t x, std::size_t y, sf::Color color)
     {
         auto index = (x * m_screenSize.y + y) * 6;
-        auto pixel_size_f = static_cast<float>(m_pixelSize); //rid us of casting in each line we use it
-        sf::Vector2f coord2d (x * pixel_size_f, y * pixel_size_f);
+        sf::Vector2f coord2d (x * m_pixelSize, y * m_pixelSize);
 
         //Triangle-1
         //top-left
@@ -29,20 +28,20 @@ namespace sn
         m_vertices[index].color    = color;
 
         //top-right
-        m_vertices[index + 1].position = coord2d + sf::Vector2f{pixel_size_f, 0};
+        m_vertices[index + 1].position = coord2d + sf::Vector2f{m_pixelSize, 0};
         m_vertices[index + 1].color = color;
 
         //bottom-right
-        m_vertices[index + 2].position = coord2d + sf::Vector2f{pixel_size_f, pixel_size_f};
+        m_vertices[index + 2].position = coord2d + sf::Vector2f{m_pixelSize, m_pixelSize};
         m_vertices[index + 2].color = color;
 
         //Triangle-2
         //bottom-right
-        m_vertices[index + 3].position = coord2d + sf::Vector2f{pixel_size_f, pixel_size_f};
+        m_vertices[index + 3].position = coord2d + sf::Vector2f{m_pixelSize, m_pixelSize};
         m_vertices[index + 3].color = color;
 
         //bottom-left
-        m_vertices[index + 4].position = coord2d + sf::Vector2f{0, pixel_size_f};
+        m_vertices[index + 4].position = coord2d + sf::Vector2f{0, m_pixelSize};
         m_vertices[index + 4].color = color;
 
         //top-left
