52f2ae1edb3c71d7725a89fd44a7ea1dbd7d49f6
Amish Naidu <amhndu@gmail.com>
2016-07-18 11:15:46 +0530

    Virtual screen vertices position setup moved

--- include/VirtualScreen.h
+++ include/VirtualScreen.h
@@ -4,18 +4,16 @@
 
 namespace sn
 {
-    using uint = unsigned int;
-
     class VirtualScreen : public sf::Drawable
     {
     public:
-        void create (uint width, uint height, float pixel_size, sf::Color default_color);
+        void create (unsigned int width, unsigned int height, float pixel_size, sf::Color color);
         void setPixel (std::size_t x, std::size_t y, sf::Color color);
 
     private:
         void draw(sf::RenderTarget& target, sf::RenderStates states) const;
 
-        sf::Vector2<uint> m_screenSize;
+        sf::Vector2u m_screenSize;
         float m_pixelSize; //virtual pixel size in real pixels
         sf::VertexArray m_vertices;
     };
--- src/VirtualScreen.cpp
+++ src/VirtualScreen.cpp
@@ -2,7 +2,7 @@
 
 namespace sn
 {
-    void VirtualScreen::create(uint w, uint h, float pixel_size, sf::Color default_color)
+    void VirtualScreen::create(unsigned int w, unsigned int h, float pixel_size, sf::Color color)
     {
         m_vertices.resize(w * h * 6);
         m_screenSize = {w, h};
@@ -12,7 +12,34 @@ namespace sn
         {
             for (std::size_t y = 0; y < h; ++y)
             {
-                setPixel(x, y, default_color);
+                auto index = (x * m_screenSize.y + y) * 6;
+                sf::Vector2f coord2d (x * m_pixelSize, y * m_pixelSize);
+
+                //Triangle-1
+                //top-left
+                m_vertices[index].position = coord2d;
+                m_vertices[index].color    = color;
+
+                //top-right
+                m_vertices[index + 1].position = coord2d + sf::Vector2f{m_pixelSize, 0};
+                m_vertices[index + 1].color = color;
+
+                //bottom-right
+                m_vertices[index + 2].position = coord2d + sf::Vector2f{m_pixelSize, m_pixelSize};
+                m_vertices[index + 2].color = color;
+
+                //Triangle-2
+                //bottom-right
+                m_vertices[index + 3].position = coord2d + sf::Vector2f{m_pixelSize, m_pixelSize};
+                m_vertices[index + 3].color = color;
+
+                //bottom-left
+                m_vertices[index + 4].position = coord2d + sf::Vector2f{0, m_pixelSize};
+                m_vertices[index + 4].color = color;
+
+                //top-left
+                m_vertices[index + 5].position = coord2d;
+                m_vertices[index + 5].color    = color;
             }
         }
     }
@@ -24,28 +51,22 @@ namespace sn
 
         //Triangle-1
         //top-left
-        m_vertices[index].position = coord2d;
         m_vertices[index].color    = color;
 
         //top-right
-        m_vertices[index + 1].position = coord2d + sf::Vector2f{m_pixelSize, 0};
         m_vertices[index + 1].color = color;
 
         //bottom-right
-        m_vertices[index + 2].position = coord2d + sf::Vector2f{m_pixelSize, m_pixelSize};
         m_vertices[index + 2].color = color;
 
         //Triangle-2
         //bottom-right
-        m_vertices[index + 3].position = coord2d + sf::Vector2f{m_pixelSize, m_pixelSize};
         m_vertices[index + 3].color = color;
 
         //bottom-left
-        m_vertices[index + 4].position = coord2d + sf::Vector2f{0, m_pixelSize};
         m_vertices[index + 4].color = color;
 
         //top-left
-        m_vertices[index + 5].position = coord2d;
         m_vertices[index + 5].color    = color;
     }
 
